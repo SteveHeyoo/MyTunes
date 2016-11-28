c82fb9958288caa269d13bf5564ef707805b9933
Stefan O <input.goes.here@gmail.com>
2016-11-28 09:59:04 +0100

    Song object created
    with; Id, title, artist, duration, filepath

--- src/mytunes/BE/Song.java
+++ src/mytunes/BE/Song.java
@@ -11,4 +11,46 @@ package mytunes.BE;
  */
 public class Song {
     
+    private final int id;
+    private String artist;
+    private String title;
+    private final String filePath;
+    private final double duration;
+
+    public Song(int id, String artist, String title, String filePath, double duration) {
+        this.id = id;
+        this.artist = artist;
+        this.title = title;
+        this.filePath = filePath;
+        this.duration = duration;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+    
 }
