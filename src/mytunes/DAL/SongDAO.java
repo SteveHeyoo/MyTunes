499b985d4d71a3b19532dcf92c8c97c9b1123257
Stefan O <input.goes.here@gmail.com>
2016-11-28 11:33:00 +0100

    Changes about how we save a song object

--- src/mytunes/DAL/SongDAO.java
+++ src/mytunes/DAL/SongDAO.java
@@ -17,7 +17,12 @@ import mytunes.BE.Song;
 public class SongDAO 
 {
     private static final int ID_SIZE = Integer.BYTES;
+    private static final int DURATION_SIZE = Double.BYTES;
     private static final int NAME_SIZE = 50;
+    private static final int FILEPATH_SIZE = 200;
+    private static final int WRITE_SIZE = ID_SIZE + DURATION_SIZE + (NAME_SIZE*2) + FILEPATH_SIZE;
+            
+    
  
     private final String fileName;
 
@@ -30,9 +35,15 @@ public class SongDAO
     {
         try (RandomAccessFile raf = new RandomAccessFile(new File(fileName), "rw"))
         {
+            int nextId = raf.readInt();
+            raf.seek(raf.length()-raf.length());
+            raf.writeInt(nextId+1);
             raf.seek(raf.length());  // place the file pointer at the end of the file.
-            raf.writeInt(s.getId());
-        //    raf.writeBytes(String.format("%-" + NAME_SIZE + "s", s.getName()).substring(0, NAME_SIZE));
+            raf.writeInt(nextId);
+            raf.writeDouble(s.getDuration());    
+            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", s.getArtist()).substring(0, NAME_SIZE));
+            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", s.getTitle()).substring(0, NAME_SIZE));
+            raf.writeBytes(String.format("%-" + FILEPATH_SIZE + "s", s.getFilePath()).substring(0, FILEPATH_SIZE));
         }
     }
 }
