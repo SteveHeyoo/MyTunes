8f5714361b8cac4c0e2cab85f4493f8e8e7b632e
Stefan O <input.goes.here@gmail.com>
2016-11-28 12:55:54 +0100

    done some more

--- src/mytunes/DAL/SongDAO.java
+++ src/mytunes/DAL/SongDAO.java
@@ -8,61 +8,83 @@ package mytunes.DAL;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Map;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import mytunes.BE.Song;
 
 /**
  *
  * @author Stefan-VpcEB3J1E
  */
-public class SongDAO 
+public class SongDAO
 {
+
     private static final int ID_SIZE = Integer.BYTES;
     private static final int DURATION_SIZE = Double.BYTES;
     private static final int NAME_SIZE = 50;
     private static final int FILEPATH_SIZE = 200;
-    private static final int WRITE_SIZE = ID_SIZE + DURATION_SIZE + (NAME_SIZE*2) + FILEPATH_SIZE;
-            
-    
- 
+    private static final int WRITE_SIZE = ID_SIZE + DURATION_SIZE + (NAME_SIZE * 2) + FILEPATH_SIZE;
+
     private final String fileName;
 
     public SongDAO(String fileName)
     {
         this.fileName = fileName;
     }
-    
-    public Song addSong(File file) throws IOException
+
+    public Song addSong(File file) throws IOException, UnsupportedAudioFileException
     {
-        int idToGive;
+        int nextId;
         double duration;
         String artist;
         String title;
-        String filePath;
-        
-        try (RandomAccessFile raff = new RandomAccessFile(file.getAbsolutePath(), "rw"))
+        String filePath = file.getAbsolutePath();
+
+        try (RandomAccessFile raff = new RandomAccessFile(filePath, "rw"))
         {
             byte[] artistByte = new byte[30];
             byte[] titleByte = new byte[30];
-            raff.seek(raff.length()-125);
+            raff.seek(raff.length() - 125);
             raff.read(titleByte);
             title = new String(titleByte).trim();
             raff.read(artistByte);
             artist = new String(artistByte).trim();
-            
-            
+
         }
         try (RandomAccessFile raf = new RandomAccessFile(new File(fileName), "rw"))
         {
-            int nextId = raf.readInt();
-            raf.seek(raf.length()-raf.length());
-            raf.writeInt(nextId+1);
+            nextId = raf.readInt();
+            raf.seek(raf.length() - raf.length());
+            raf.writeInt(nextId + 1);
             raf.seek(raf.length());  // place the file pointer at the end of the file.
             raf.writeInt(nextId);
-//            raf.writeDouble(s.getDuration());    
-//            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", s.getArtist()).substring(0, NAME_SIZE));
-//            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", s.getTitle()).substring(0, NAME_SIZE));
-//            raf.writeBytes(String.format("%-" + FILEPATH_SIZE + "s", s.getFilePath()).substring(0, FILEPATH_SIZE));
+            duration = getDuration(file);
+            raf.writeDouble(duration);    
+            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", artist).substring(0, NAME_SIZE));
+            raf.writeBytes(String.format("%-" + NAME_SIZE + "s", title).substring(0, NAME_SIZE));
+            raf.writeBytes(String.format("%-" + FILEPATH_SIZE + "s", filePath).substring(0, FILEPATH_SIZE));
         }
-        return null;
+        return (new Song(nextId, artist, title, filePath, duration));
     }
+
+    private double getDuration(File file) throws IOException, UnsupportedAudioFileException
+    {
+
+        AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+
+        Map<?, ?> properties = fileFormat.properties();
+        String key = "duration";
+        Long microseconds = (Long) properties.get(key);
+        int mili = (int) (microseconds / 1000);
+        double sec = (double) (mili / 1000);
+        return sec;
+
+//            int sec = (mili / 1000) % 60;
+//            int min = (mili / 1000) / 60;
+    }
+    
+
+
 }
